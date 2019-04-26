5fe8173eadc2d404f5d30ff0f912cb4731ee2dec
mutan <akim_now@mail.ru>
2019-04-27 01:36:02 +0300

    Working

--- src/Command/PassportUpdateCommand.php
+++ src/Command/PassportUpdateCommand.php
@@ -11,6 +11,14 @@ class PassportUpdateCommand extends Command
 {
     const SOURCE_URL = 'https://guvm.mvd.ru/upload/expired-passports/list_of_expired_passports.csv.bz2';
 
+    private $projectDir;
+
+    public function __construct($projectDir)
+    {
+        $this->projectDir = $projectDir;
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this->setName('app:passport:update')
@@ -22,6 +30,13 @@ class PassportUpdateCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
+        $versionFile = $this->projectDir . implode(DIRECTORY_SEPARATOR, ['', 'var', 'data', 'version.txt']);
+        //file_put_contents($versionFile, date("Y-m-d H:i:s"));
+
+
+        $headers = get_headers(self::SOURCE_URL, 1);
+        
+        dump($versionFile); die('ok');
 
 
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
